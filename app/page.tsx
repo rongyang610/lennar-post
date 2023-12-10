82854b530a265959689eed05c1a75296839c9c0f
rongyang610 <rongyang610@gmail.com>
2023-12-10 16:54:17 -0600

    feat(home-page): update css and add mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+import EmailForm from '@/components/EmailForm'
+
 export default function Home() {
   return (
-    <section className="flex h-full flex-col justify-center bg-[url('/illustration.png')] bg-auto bg-right bg-no-repeat">
+    <section className="flex h-full flex-col justify-center bg-[url('/illustration.png')] bg-auto bg-right bg-no-repeat gap-6">
       <div className="max-w-[592px]">
         <div className="flex w-fit gap-4 rounded-[14px] bg-black py-1 pl-1 pr-2">
           <h5 className="h-fit rounded-[10px] bg-[#6366F1] px-3 py-0.5 text-xs">
@@ -19,10 +21,10 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex flex-col gap-5">
-          <h1 className="flex flex-col text-6xl">
+        <div className="flex flex-col gap-5 mb-6">
+          <h1 className="flex flex-col text-6xl font-extrabold mb-5">
             A better way to
-            <span>ship web apps</span>
+            <span className="text-[#818CF8]">ship web apps</span>
           </h1>
           <p>
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
@@ -30,15 +32,15 @@ export default function Home() {
             fugiat.
           </p>
         </div>
-        <div>
-          <form>
-            <input type="email" placeholder="Enter your email" />
-            <button type="submit">Start free trial</button>
-          </form>
-          <span>
+        <div className="flex flex-col gap-4">
+          <EmailForm />
+          <span className="text-[#6B7280]">
             Start your free 14-day trial, no credit card necessary. By providing
             your email, you agree to our{' '}
-            <Link href="/terms">terms or service</Link>.
+            <Link href="/terms" className="text-white">
+              terms or service
+            </Link>
+            .
           </span>
         </div>
       </div>
