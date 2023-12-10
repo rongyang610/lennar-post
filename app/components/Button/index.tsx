d2c42b1bb095854bb20ff35a1bd9d49003b744fb
rongyang610 <rongyang610@gmail.com>
2023-12-10 16:31:09 -0600

    feat(button): update button component

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,8 +1,10 @@
 import { ButtonProps } from './types'
 
-export default function Button({ text }: ButtonProps) {
+export default function Button({ classNames = '', text }: ButtonProps) {
   return (
-    <button className="w-full sm:w-fit px-4 py-3 rounded-md bg-[#6366F1] text-white">
+    <button
+      className={`w-full sm:w-fit px-4 py-3 rounded-md bg-[#6366F1] text-white ${classNames}`}
+    >
       {text}
     </button>
   )
