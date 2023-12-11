77159b73804609168ffc49e93595249ff246b8d8
rongyang610 <rongyang610@gmail.com>
2023-12-10 21:46:51 -0600

    style(navigation): use fixed instead of absolute

--- app/components/Navigation/Mobile.tsx
+++ app/components/Navigation/Mobile.tsx
@@ -11,7 +11,7 @@ const MobileNavigation = () => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const animationClasses = show
-    ? 'top-1 visible opacity-1 duration-500'
+    ? 'top-0 visible opacity-1 duration-500'
     : 'top-[-40vh] invisible opacity-0 duration-300'
 
   return (
@@ -26,42 +26,44 @@ const MobileNavigation = () => {
       />
       {show && (
         <div
-          className="absolute opacity-75 h-screen z-10 top-0 left-0 w-full bg-[#6B7280]"
+          className="fixed opacity-75 h-screen z-10 top-0 left-0 w-full bg-[#6B7280]"
           onClick={handleClose}
         />
       )}
       <div
-        className={`flex flex-col absolute z-20 px-5 pt-4 pb-6 left-0 bg-white w-[98%] rounded-lg ml-1 transition-all ease-in-out ${animationClasses}`}
+        className={`fixed w-full z-20 px-1 pt-1 transition-all ease-in-out left-0 ${animationClasses}`}
       >
-        <div className="flex justify-between w-full mb-8">
-          <Image alt="logo" src="/logo.svg" width={35} height={32} />
-          <Image
-            alt="close"
-            src="/close.svg"
-            width={24}
-            height={24}
-            onClick={handleClose}
-            className="hover:cursor-pointer"
-          />
-        </div>
-        <div className="flex flex-col gap-5 mb-8">
-          {NAVIGATION_ITEMS_LEFT.map(({ slug, title }, key) => (
-            <Link
-              href={slug}
-              key={slug + title + key}
-              className="text-[#374151] w-fit"
-            >
-              {title}
-            </Link>
-          ))}
-        </div>
-        <div className="flex flex-col gap-6">
-          <Button text="Start free trial" />
-          <div className="flex justify-center">
-            <span className="text-[#6B7280]">Existing customer?</span>
-            <Link href="/login" className="text-[#111827] ml-1">
-              Login
-            </Link>
+        <div className="flex flex-col px-5 pt-4 pb-6 bg-white rounded-lg">
+          <div className="flex justify-between w-full mb-8">
+            <Image alt="logo" src="/logo.svg" width={35} height={32} />
+            <Image
+              alt="close"
+              src="/close.svg"
+              width={24}
+              height={24}
+              onClick={handleClose}
+              className="hover:cursor-pointer"
+            />
+          </div>
+          <div className="flex flex-col gap-5 mb-8">
+            {NAVIGATION_ITEMS_LEFT.map(({ slug, title }, key) => (
+              <Link
+                href={slug}
+                key={slug + title + key}
+                className="text-[#374151] w-fit"
+              >
+                {title}
+              </Link>
+            ))}
+          </div>
+          <div className="flex flex-col gap-6">
+            <Button text="Start free trial" />
+            <div className="flex justify-center">
+              <span className="text-[#6B7280]">Existing customer?</span>
+              <Link href="/login" className="text-[#111827] ml-1">
+                Login
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -5,7 +5,7 @@ import DesktopNavigation from './Desktop'
 import MobileNavigation from './Mobile'
 
 const Navigation = () => (
-  <nav className="flex justify-between items-center mr-1 sm:px-1">
+  <nav className="flex justify-between items-center mr-1 sm:px-1 relative">
     <Link href="/">
       <Image alt="logo" src="/logo.svg" width={44} height={40} />
     </Link>
